6101c9b7842130e1a8bc614fa064ec79c90de23a
Kyle Conroy <kyle@conroy.org>
2022-04-02 20:02:50 -0700

    feat: Upgrade to the Node 16 runtime
    Also upgrade all of the dependencies

--- src/main.ts
+++ src/main.ts
@@ -47,7 +47,7 @@ async function run(): Promise<void> {
       }
     }
   } catch (error) {
-    core.setFailed(error.message)
+    if (error instanceof Error) core.setFailed(error.message)
   }
 }
 
