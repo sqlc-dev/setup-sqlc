e868341a41611f703fc537bb9a924cc675e64ac7
Kyle Conroy <kyle@conroy.org>
2023-09-06 10:46:10 -0700

    Support ARM

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,47 @@
 import * as core from '@actions/core'
 import * as tc from '@actions/tool-cache'
 
+function downloadUrl(version: string): string {
+  let plat = ''
+  let arch = ''
+
+  switch (process.platform) {
+    case 'win32': {
+      plat = 'windows'
+      break
+    }
+    case 'darwin': {
+      plat = 'darwin'
+      break
+    }
+    case 'linux': {
+      plat = 'linux'
+      break
+    }
+    default: {
+      core.setFailed(`Unsupported platform: ${process.platform}`)
+      return ''
+    }
+  }
+
+  switch (process.arch) {
+    case 'x64': {
+      arch = 'amd64'
+      break
+    }
+    case 'arm64': {
+      arch = 'arm64'
+      break
+    }
+    default: {
+      core.setFailed(`Unsupported architecture: ${process.arch}`)
+      return ''
+    }
+  }
+
+  return `https://downloads.sqlc.dev/sqlc_${version}_${plat}_${arch}.zip`
+}
+
 async function run(): Promise<void> {
   try {
     const version = core.getInput('sqlc-version')
@@ -8,44 +49,22 @@ async function run(): Promise<void> {
       core.setFailed(`sqlc-version not set`)
       return
     }
-    const toolDir = tc.find('sqlc', version, 'x64')
+    const toolDir = tc.find('sqlc', version, process.arch)
     if (toolDir !== '') {
       core.addPath(toolDir)
       return
     }
 
-    switch (process.platform) {
-      case 'win32': {
-        const toolUrl = `https://downloads.sqlc.dev/sqlc_${version}_windows_amd64.zip`
-        const downloadPath = await tc.downloadTool(toolUrl)
-        const extPath = await tc.extractZip(downloadPath)
-        const cachedPath = await tc.cacheDir(extPath, 'sqlc', version)
-        core.addPath(cachedPath)
-        break
-      }
-
-      case 'darwin': {
-        const toolUrl = `https://downloads.sqlc.dev/sqlc_${version}_darwin_amd64.zip`
-        const downloadPath = await tc.downloadTool(toolUrl)
-        const extPath = await tc.extractZip(downloadPath)
-        const cachedPath = await tc.cacheDir(extPath, 'sqlc', version)
-        core.addPath(cachedPath)
-        break
-      }
-
-      case 'linux': {
-        const toolUrl = `https://downloads.sqlc.dev/sqlc_${version}_linux_amd64.zip`
-        const downloadPath = await tc.downloadTool(toolUrl)
-        const extPath = await tc.extractZip(downloadPath)
-        const cachedPath = await tc.cacheDir(extPath, 'sqlc', version)
-        core.addPath(cachedPath)
-        break
-      }
-
-      default: {
-        core.setFailed(`Unsupported platform: ${process.platform}`)
-      }
+    const toolUrl = downloadUrl(version)
+    if (toolUrl === '') {
+      return
     }
+
+    const downloadPath = await tc.downloadTool(toolUrl)
+    const extPath = await tc.extractZip(downloadPath)
+    const cachedPath = await tc.cacheDir(extPath, 'sqlc', version)
+    core.info(cachedPath)
+    core.addPath(cachedPath)
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
